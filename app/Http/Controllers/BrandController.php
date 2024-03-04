21c832daed29ee9d0dd13e0d69ac285eabb7ad59
Willian <williannunes965@gmail.com>
2024-03-04 14:51:42 -0300

    configurating api routes and brandcontroller

--- app/Http/Controllers/BrandController.php
+++ app/Http/Controllers/BrandController.php
@@ -7,59 +7,57 @@ use Illuminate\Http\Request;
 
 class BrandController extends Controller
 {
+    public $brand;
+    public function __construct(Brand $brand)
+    {
+        $this->brand = $brand;   
+    }
     /**
      * Display a listing of the resource.
      */
     public function index()
     {
-        //
+        $brands = $this->brand->all();
+        return $brands;
     }
 
-    /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        //
-    }
 
     /**
      * Store a newly created resource in storage.
      */
     public function store(Request $request)
     {
-        //
+        $newBrand = new Brand();
+        $newBrand->fill($request->all());
+        $newBrand->save();
+        return $newBrand;
     }
 
     /**
      * Display the specified resource.
      */
-    public function show(Brand $brand)
+    public function show($brandId)
     {
-        //
+        $brand = $this->brand->find($brandId);
+        return $brand;
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(Brand $brand)
-    {
-        //
-    }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, Brand $brand)
+    public function update(Request $request, $brandId)
     {
-        //
+        $brand = $this->brand->find($brandId);
+        $brand->update($request->all());
+        return $brand;
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(Brand $brand)
+    public function destroy($brandId)
     {
-        //
+        return $this->brand->destroy($brandId);
     }
 }
