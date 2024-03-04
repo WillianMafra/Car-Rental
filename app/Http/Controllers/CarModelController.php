8af04d4c54f0a768e22b37f9eb261debfbd9b5f9
Willian <willnmafra@DESKTOP-AHRL1NB.localdomain>
2024-03-04 11:36:14 -0300

    first commit

---
+++ app/Http/Controllers/CarModelController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\carModel;
+use Illuminate\Http\Request;
+
+class CarModelController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(carModel $carModel)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(carModel $carModel)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, carModel $carModel)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(carModel $carModel)
+    {
+        //
+    }
+}
